6a505f27f54b0be1e052a5bc7dfbe6a7f1644a8e
mousa <mohammad.mousa@doit.com.sa>
2021-06-05 18:27:22 +0200

    first commit

---
+++ test-ivy/src/core/Main.java
@@ -0,0 +1,57 @@
+package core;
+
+import java.util.Date;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Main implements ApplicationListener {
+
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 700;
+		config.height = 250;
+		config.title = "Ivy[" + new Date().toGMTString() + "]";
+		new LwjglApplication(new Main(), config);
+	}
+
+	private SpriteBatch batch;
+	private Texture img;
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		img = new Texture("kartngo.png");
+	}
+
+	@Override
+	public void render() {
+		ScreenUtils.clear(1, 1, 1, 1);
+		batch.begin();
+		batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+		img.dispose();
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+}
